91d889e13e3be229afa5b0a4aa63fb485024b92a
jamesshedden <james@jshedden.com>
2017-04-04 22:19:30 +0100

    Adding fade transitions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import _ from 'lodash';
 
+// TODO
+//
+// Prevent anymore transitions on shrinking circles
+// Once circles start getting deleted, think COUNTER is messing up the distances
+
 const contentElement = document.getElementById('content');
 const randomNumber = (min, max) => Math.floor(Math.random() * max) + min;
-let COUNTER = 1;
+let COUNTER = 0;
 const MAX_CIRCLE_AMOUNT = 20;
 
 const COLORS = [
@@ -17,37 +22,61 @@ const makeCircle = (event) => {
   const circle = document.createElement('div');
   circle.id = `circle-${ COUNTER }`;
   circle.className = 'circle';
-
-  COUNTER++;
+  circle.classList.add('has-transitions');
 
   const randomDimension = randomNumber(100, 250);
 
-  // The top, left, translateX & translateY values should be set
-  // to ensure that the sphere appears when the user clicked, but its position
-  // is correctly transformed.
+  const coordinatesFromCentre = {
+    x: event.pageX - (window.innerWidth / 2),
+    y: (window.innerHeight / 2) - event.pageY,
+  };
 
-  // For the sphere to appear where the user clicked, its position should be:
-  // Its absolute position + any transformations based on the coords of the click
+  const counterForTranslate = COUNTER > MAX_CIRCLE_AMOUNT ? MAX_CIRCLE_AMOUNT : COUNTER;
+
+  const translateXAmount = -(coordinatesFromCentre.x * (counterForTranslate + 2)) / 100;
+  const translateYAmount = (coordinatesFromCentre.y * (counterForTranslate + 2)) / 100;
+
+  COUNTER++;
 
   circle.setAttribute('style', `
     width: ${ randomDimension }px;
     height: ${ randomDimension }px;
     position: fixed;
-    top: ${ event.pageY - (randomDimension / 2) }px;
-    left: ${ event.pageX - (randomDimension / 2) }px;
+    opacity: 0;
+    transform: translateX(${ translateXAmount }px) translateY(${ translateYAmount }px);
+    top: ${ event.pageY - translateYAmount - (randomDimension / 2) }px;
+    left: ${ event.pageX - translateXAmount - (randomDimension / 2) }px;
     background: linear-gradient(${ randomNumber(0, 90) }deg, #FEB522 0%, ${ COLORS[randomNumber(0, COLORS.length)] } 100%);
   `);
 
   if (COUNTER > MAX_CIRCLE_AMOUNT) {
     const circleToDelete = document.getElementById(`circle-${ COUNTER - MAX_CIRCLE_AMOUNT }`);
-    circleToDelete.classList.add('scale-down');
-    setTimeout(() => circleToDelete.remove(), 990);
+    // const toDeleteTranslateXAmount = -(coordinatesFromCentre.x * (COUNTER - MAX_CIRCLE_AMOUNT + 2)) / 100;
+    // const toDeleteTranslateYAmount = (coordinatesFromCentre.y * (COUNTER - MAX_CIRCLE_AMOUNT + 2)) / 100;
+
+    setTimeout(() => {
+      circleToDelete.classList.add('has-slow-transitions');
+      circleToDelete.style.opacity = '0';
+
+      setTimeout(() => {
+        circleToDelete.remove();
+      }, 990);
+    }, 50);
   }
 
   contentElement.appendChild(circle);
+
+  setTimeout(() => {
+    circle.style.opacity = '1';
+
+    setTimeout(() => {
+      circle.classList.remove('has-transitions');
+    }, 400);
+  }, 50);
 };
 
 const mouseMove = (event) => {
+  console.log('mouseMove()');
   const coordinatesFromCentre = {
     x: event.pageX - (window.innerWidth / 2),
     y: (window.innerHeight / 2) - event.pageY,
@@ -58,8 +87,6 @@ const mouseMove = (event) => {
   _.map(circles, (circle, index) => {
     const translateXAmount = -(coordinatesFromCentre.x * (index + 2)) / 100;
     const translateYAmount = (coordinatesFromCentre.y * (index + 2)) / 100;
-
-    // console.log('translateXAmount, translateYAmount', translateXAmount, translateYAmount);
 
     circle.style.transform = `
       scale(1) translateX(${ translateXAmount }px) translateY(${ translateYAmount }px)
