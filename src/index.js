4fa81105aab4049b0e23afac99371f99964bc7be
jamesshedden <james@jshedden.com>
2017-04-04 21:02:20 +0100

    Adding parallax circles

--- src/index.js
+++ src/index.js
@@ -1,49 +1,73 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 
 const contentElement = document.getElementById('content');
 const randomNumber = (min, max) => Math.floor(Math.random() * max) + min;
 let COUNTER = 1;
+const MAX_CIRCLE_AMOUNT = 20;
 
 const COLORS = [
-  'lightblue',
-  'lightgreen',
-  'yellow',
-  'orange',
-  'purple',
-  'pink',
+  '#FF9E9E',
+  '#9EFFC6',
+  '#9EEFFF',
+  '#D8CEFF',
+  '#B6FF9E',
 ];
 
-const makeCircles = (posX, posY, discElement) => {
+const makeCircle = (event) => {
   const circle = document.createElement('div');
+  circle.id = `circle-${ COUNTER }`;
   circle.className = 'circle';
+
+  COUNTER++;
+
   const randomDimension = randomNumber(100, 250);
+
+  // The top, left, translateX & translateY values should be set
+  // to ensure that the sphere appears when the user clicked, but its position
+  // is correctly transformed.
+
+  // For the sphere to appear where the user clicked, its position should be:
+  // Its absolute position + any transformations based on the coords of the click
 
   circle.setAttribute('style', `
     width: ${ randomDimension }px;
     height: ${ randomDimension }px;
     position: fixed;
-    top: ${ posY - (randomDimension / 2) }px;
-    left: ${ posX - (randomDimension / 2) }px;
-    background: ${ COLORS[randomNumber(0, COLORS.length)] };
+    top: ${ event.pageY - (randomDimension / 2) }px;
+    left: ${ event.pageX - (randomDimension / 2) }px;
+    background: linear-gradient(${ randomNumber(0, 90) }deg, #FEB522 0%, ${ COLORS[randomNumber(0, COLORS.length)] } 100%);
   `);
 
-  discElement.appendChild(circle);
-};
-
-const makeDisc = (event) => {
-  const disc = document.createElement('div');
-  disc.id = `disc-${ COUNTER }`;
-
-  [...Array(5)].map(() => makeCircles(event.pageX, event.pageY, disc));
-  COUNTER++;
-
-  if (COUNTER > 15) {
-    const discToDelete = document.getElementById(`disc-${ COUNTER - 15 }`);
-    discToDelete.classList.add('scale-down');
-    setTimeout(() => discToDelete.remove(), 1000);
+  if (COUNTER > MAX_CIRCLE_AMOUNT) {
+    const circleToDelete = document.getElementById(`circle-${ COUNTER - MAX_CIRCLE_AMOUNT }`);
+    circleToDelete.classList.add('scale-down');
+    setTimeout(() => circleToDelete.remove(), 990);
   }
 
-  contentElement.appendChild(disc);
+  contentElement.appendChild(circle);
 };
 
-contentElement.addEventListener('click', makeDisc, true);
+const mouseMove = (event) => {
+  const coordinatesFromCentre = {
+    x: event.pageX - (window.innerWidth / 2),
+    y: (window.innerHeight / 2) - event.pageY,
+  };
+
+  const circles = document.getElementsByClassName('circle');
+
+  _.map(circles, (circle, index) => {
+    const translateXAmount = -(coordinatesFromCentre.x * (index + 2)) / 100;
+    const translateYAmount = (coordinatesFromCentre.y * (index + 2)) / 100;
+
+    // console.log('translateXAmount, translateYAmount', translateXAmount, translateYAmount);
+
+    circle.style.transform = `
+      scale(1) translateX(${ translateXAmount }px) translateY(${ translateYAmount }px)
+    `;
+  });
+};
+
+const throttledMouseMove = _.throttle(mouseMove, 10);
+
+contentElement.addEventListener('click', makeCircle, true);
+contentElement.addEventListener('mousemove', throttledMouseMove, true);
