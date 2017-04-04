2ad79cb0d083041abcf05b8edd527e55fcdff7f8
jamesshedden <james@jshedden.com>
2017-04-04 22:32:49 +0100

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import _ from 'lodash';
 
-// TODO
-//
-// Prevent anymore transitions on shrinking circles
-// Once circles start getting deleted, think COUNTER is messing up the distances
-
 const contentElement = document.getElementById('content');
 const randomNumber = (min, max) => Math.floor(Math.random() * max) + min;
-let COUNTER = 0;
-const MAX_CIRCLE_AMOUNT = 20;
 
-const COLORS = [
-  '#FF9E9E',
-  '#9EFFC6',
-  '#9EEFFF',
-  '#D8CEFF',
-  '#B6FF9E',
-];
+let COUNTER = 0;
+
+const MAX_CIRCLE_AMOUNT = 20;
+const PARALLAX_AMOUNT_DIVISOR = 80;
+const LONG_ANIMATION_DURATION = 1000;
+const SHORT_ANIMATION_DURATION = 200;
+
+const COLORS = ['#FF9E9E', '#9EFFC6', '#9EEFFF', '#D8CEFF', '#B6FF9E'];
 
 const makeCircle = (event) => {
   const circle = document.createElement('div');
@@ -32,9 +26,8 @@ const makeCircle = (event) => {
   };
 
   const counterForTranslate = COUNTER > MAX_CIRCLE_AMOUNT ? MAX_CIRCLE_AMOUNT : COUNTER;
-
-  const translateXAmount = -(coordinatesFromCentre.x * (counterForTranslate + 2)) / 100;
-  const translateYAmount = (coordinatesFromCentre.y * (counterForTranslate + 2)) / 100;
+  const translateXAmount = -(coordinatesFromCentre.x * (counterForTranslate + 2)) / PARALLAX_AMOUNT_DIVISOR;
+  const translateYAmount = (coordinatesFromCentre.y * (counterForTranslate + 2)) / PARALLAX_AMOUNT_DIVISOR;
 
   COUNTER++;
 
@@ -51,32 +44,23 @@ const makeCircle = (event) => {
 
   if (COUNTER > MAX_CIRCLE_AMOUNT) {
     const circleToDelete = document.getElementById(`circle-${ COUNTER - MAX_CIRCLE_AMOUNT }`);
-    // const toDeleteTranslateXAmount = -(coordinatesFromCentre.x * (COUNTER - MAX_CIRCLE_AMOUNT + 2)) / 100;
-    // const toDeleteTranslateYAmount = (coordinatesFromCentre.y * (COUNTER - MAX_CIRCLE_AMOUNT + 2)) / 100;
 
     setTimeout(() => {
       circleToDelete.classList.add('has-slow-transitions');
       circleToDelete.style.opacity = '0';
-
-      setTimeout(() => {
-        circleToDelete.remove();
-      }, 990);
-    }, 50);
+      setTimeout(() => circleToDelete.remove(), LONG_ANIMATION_DURATION);
+    }, 0);
   }
 
   contentElement.appendChild(circle);
 
   setTimeout(() => {
     circle.style.opacity = '1';
-
-    setTimeout(() => {
-      circle.classList.remove('has-transitions');
-    }, 400);
-  }, 50);
+    setTimeout(() => circle.classList.remove('has-transitions'), SHORT_ANIMATION_DURATION);
+  }, 0);
 };
 
 const mouseMove = (event) => {
-  console.log('mouseMove()');
   const coordinatesFromCentre = {
     x: event.pageX - (window.innerWidth / 2),
     y: (window.innerHeight / 2) - event.pageY,
@@ -85,8 +69,8 @@ const mouseMove = (event) => {
   const circles = document.getElementsByClassName('circle');
 
   _.map(circles, (circle, index) => {
-    const translateXAmount = -(coordinatesFromCentre.x * (index + 2)) / 100;
-    const translateYAmount = (coordinatesFromCentre.y * (index + 2)) / 100;
+    const translateXAmount = -(coordinatesFromCentre.x * (index + 2)) / PARALLAX_AMOUNT_DIVISOR;
+    const translateYAmount = (coordinatesFromCentre.y * (index + 2)) / PARALLAX_AMOUNT_DIVISOR;
 
     circle.style.transform = `
       scale(1) translateX(${ translateXAmount }px) translateY(${ translateYAmount }px)
@@ -95,6 +79,5 @@ const mouseMove = (event) => {
 };
 
 const throttledMouseMove = _.throttle(mouseMove, 10);
-
 contentElement.addEventListener('click', makeCircle, true);
 contentElement.addEventListener('mousemove', throttledMouseMove, true);
